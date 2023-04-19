ba8edc88bd617ee736e439d156d598c5533322ed
JuanM <juanmanuelgutierrezgarcia@outlook.com>
2023-04-19 12:36:34 -0600

    jm create basic layout

---
+++ main.js
@@ -0,0 +1,12 @@
+const toggleSidebarButton = document.querySelector("#toggle-sidebar");
+const container = document.querySelector(".container");
+
+toggleSidebarButton.addEventListener("click", function () {
+  container.classList.toggle("container--sidebar-hidden");
+});
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navLinks = document.querySelector("nav ul");
+
+hamburgerMenu.addEventListener("click", function () {
+  navLinks.classList.toggle("show");
+});
